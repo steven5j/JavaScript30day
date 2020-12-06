8de818cda36da5b051ae052b14ebdda02447cd10
steven_jhu <stevenjhu5j@Gmail.com>
2020-12-06 12:29:28 +0800

    更新json檔和把主頁撰寫

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ const githubUrl = "https://github.com/steven5j/JavaScript30day/tree/main/";
 
 const couseList = document.querySelector(".courseList");
 
-fetch("postData.json")
+fetch("https://steven5j.github.io/JavaScript30day/postData.json")
   .then(blob => blob.json())
   .then(data => {
     postDatas.push(...data);
     createView();
+    Steven_createView();
   });
 
 /** 畫面建立 */
@@ -33,4 +34,23 @@ function createView() {
               </li>`);
   });
   document.querySelector(".courseList").innerHTML = view.join("");
+}
+
+
+function Steven_createView() {
+  let view = [];
+  postDatas.forEach(data => {
+    view.push(`   <a href="${blogUrl}">
+                    <li class="steven-card">
+                      <img src="${data.img}" alt="">
+                      <h3>[Day${data.no}] ${data.title}</h3>
+                      <p>${data.desc}</p>
+                      <a class="steven_course_btn" href="${blogUrl}JavaScript30day/${data.github}" target="_blank" >Demo</a>
+                      <a class="steven_course_btn" href="${blogUrl +
+                    data.blog}" target="_blank" >筆記</a>
+                    </li>
+                  </a>
+                  `);
+  });
+  document.querySelector(".steven_card_box").innerHTML = view.join("");
 }
