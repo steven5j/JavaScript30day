4e4be245d8c74a5d07f1b9babcc8d4cff4e18b04
steven_jhu <stevenjhu5j@Gmail.com>
2020-12-06 13:41:59 +0800

    更新首頁，調整BUG

--- script.js
+++ script.js
@@ -11,31 +11,8 @@ fetch("https://steven5j.github.io/JavaScript30day/postData.json")
   .then(blob => blob.json())
   .then(data => {
     postDatas.push(...data);
-    createView();
     Steven_createView();
   });
-
-/** 畫面建立 */
-function createView() {
-  let view = [];
-  postDatas.forEach(data => {
-    view.push(`<li>
-                <div class="course">
-                  <h2 class="course__title">第 ${data.no} 天 - ${
-      data.title
-    }</h2>
-                  <div class="course__desc">${data.desc}</div>
-                  <a class="course__btn" href="${blogUrl}JavaScript30day/${data.github}" target="_blank" >Demo</a>
-                  <a class="course__btn" href="${blogUrl +
-                    data.blog}" target="_blank" >筆記</a>
-                  <a class="course__btn" href="${githubUrl +
-                    data.github}" target="_blank">Source Code</a>
-                </div>
-              </li>`);
-  });
-  document.querySelector(".courseList").innerHTML = view.join("");
-}
-
 
 function Steven_createView() {
   let view = [];
